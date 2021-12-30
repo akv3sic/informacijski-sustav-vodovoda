a96eb572f81e860b318e568da2eee5beb2086447
Ante Kvesić <antekvesic2@gmail.com>
2021-12-30 15:05:34 +0100

    Add features section

---
+++ web-app/src/components/home/FeaturesPresentation.vue
@@ -0,0 +1,41 @@
+<template>
+    <section id="features">
+        <v-container class="">
+            <v-row class="py-5" align="center">
+                <v-col cols="12" md="4" v-for="feature in features" :key="feature.id">
+                    <v-card
+                        :class="{'mx-auto': $vuetify.breakpoint.smAndDown}"
+                        max-width="344"
+                        flat
+                    >
+                        <v-card-text>
+                            <v-icon>{{ feature.icon }}</v-icon>
+                        </v-card-text>
+                    
+
+                        <v-card-title>
+                            {{ feature.name }}
+                        </v-card-title>
+
+                        <v-card-subtitle>
+                            {{ feature.description }}
+                        </v-card-subtitle>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "FeaturesPresentation",
+    data: () => ({
+        features: [
+            { id: 1, name: "Feature Name", description: "Odio pellentesque diam volutpat commodo sed egestas. Turpis nunc eget lorem dolor sed.", icon: "mdi-domain" },
+            { id: 2, name: "Feature Name", description: "Odio pellentesque diam volutpat commodo sed egestas. Turpis nunc eget lorem dolor sed.", icon: "mdi-domain" },
+            { id: 3, name: "Feature Name", description: "Odio pellentesque diam volutpat commodo sed egestas. Turpis nunc eget lorem dolor sed.", icon: "mdi-domain" },
+        ]
+    })
+}
+</script>
--- web-app/src/views/Home.vue
+++ web-app/src/views/Home.vue
@@ -2,18 +2,20 @@
     <div>
       <Header/>
       <Hero />
+      <FeaturesPresentation />
     </div>
 </template>
 
 <script>
 import Header from "@/components/layout/TheHeader"
 import Hero from "@/components/home/TheHero"
+import FeaturesPresentation  from "@/components/home/FeaturesPresentation"
 
   export default {
     name: 'Home',
 
     components: {
-      Header, Hero
+      Header, Hero, FeaturesPresentation
     },
   }
 </script>

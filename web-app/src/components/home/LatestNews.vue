869e0e05a4332a582a1114e193cb234181a86124
Ante Kvesić <antekvesic2@gmail.com>
2022-01-02 23:30:11 +0100

    Add latest news preview

---
+++ web-app/src/components/home/LatestNews.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <h2
+                class="text--center display-1"
+            >
+                Pregled novosti
+            </h2>
+        </v-row>
+        <v-row class="mt-8">
+                <LatestNewsItem 
+                    v-for="post in posts"
+                    :post=post
+                    :key="post.id"
+                />
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import LatestNewsItem from './LatestNewsItem.vue'
+
+export default {
+    name: "LatestNews",
+    components: {LatestNewsItem},
+    data: () => ({
+        posts: [
+            {id: 1, title:"Post title", content: "Adipiscing elit duis tristique sollicitudin nibh. Sed faucibus turpis in eu mi bibendum. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Eu non diam phasellus vestibulum lorem sed. Maecenas volutpat blandit aliquam etiam erat velit.", img: "https://www.blogtyrant.com/wp-content/uploads/2017/02/how-to-write-a-good-blog-post.png"}, 
+            {id: 2, title:"Post title", content: "Adipiscing elit duis tristique sollicitudin nibh. Sed faucibus turpis in eu mi bibendum. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Eu non diam phasellus vestibulum lorem sed. Maecenas volutpat blandit aliquam etiam erat velit.", img : "https://www.blogtyrant.com/wp-content/uploads/2017/02/how-to-write-a-good-blog-post.png"}, 
+            {id: 3, title:"Post title", content: "Adipiscing elit duis tristique sollicitudin nibh. Sed faucibus turpis in eu mi bibendum. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Eu non diam phasellus vestibulum lorem sed. Maecenas volutpat blandit aliquam etiam erat velit.", img : "https://www.blogtyrant.com/wp-content/uploads/2017/02/how-to-write-a-good-blog-post.png"}, 
+            {id: 4, title:"Post title", content: "Adipiscing elit duis tristique sollicitudin nibh. Sed faucibus turpis in eu mi bibendum. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Eu non diam phasellus vestibulum lorem sed. Maecenas volutpat blandit aliquam etiam erat velit.", img: "https://www.blogtyrant.com/wp-content/uploads/2017/02/how-to-write-a-good-blog-post.png"}       
+            ]
+    })
+}
+</script>
--- web-app/src/views/Home.vue
+++ web-app/src/views/Home.vue
@@ -3,6 +3,7 @@
       <Header/>
       <Hero />
       <FeaturesPresentation />
+      <LatestNews />
     </div>
 </template>
 
@@ -10,12 +11,13 @@
 import Header from "@/components/layout/TheHeader"
 import Hero from "@/components/home/TheHero"
 import FeaturesPresentation  from "@/components/home/FeaturesPresentation"
+import LatestNews from '../components/home/LatestNews.vue'
 
   export default {
     name: 'Home',
 
     components: {
-      Header, Hero, FeaturesPresentation
+      Header, Hero, FeaturesPresentation, LatestNews
     },
   }
 </script>

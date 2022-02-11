427fce4bd8acb020c3e80935d0d1773f05d83c1d
Ante Kvesić <antekvesic2@gmail.com>
2022-02-11 16:59:35 +0100

    Add form for changing consuption categories and connect to api

--- web-app/src/store/index.js
+++ web-app/src/store/index.js
@@ -6,11 +6,12 @@ import auth from './modules/auth'
 import userAccountDetails from './modules/userAccountDetails'
 import userBills from './modules/userBills'
 import userConsuptionOverview from './modules/userConsuptionOverview'
+import adminConsuptionCategories from './modules/adminConsuptionCategories'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    admin, userDashboard, auth, userAccountDetails, userBills, userConsuptionOverview
+    admin, userDashboard, auth, userAccountDetails, userBills, userConsuptionOverview, adminConsuptionCategories
   }
 })
--- web-app/src/views/admin/ConsuptionCategories.vue
+++ web-app/src/views/admin/ConsuptionCategories.vue
@@ -4,7 +4,11 @@
             <v-col>
                 <span class="text-h5">Kategorije potrošnje</span>
                 <div class="ml-2 d-inline">
-                    <v-btn text small class="primary--text" to="proizvodi-dodaj-novi">Promijeni kategorizaciju</v-btn>   
+                    <v-btn text small class="primary--text" v-if="!changeActivated" @click="activateChange">Promijeni kategorizaciju</v-btn>
+                    <span v-else>
+                        <v-btn text small class="primary--text" @click="changeCategories">Spremi promjene</v-btn>   
+                        <v-btn text small class="red--text" @click="cancelChanges">Odustani</v-btn>   
+                    </span>   
                 </div>
             </v-col>
         </v-row>
@@ -21,11 +25,19 @@
                         1. kategorija
                     </v-card-title>
 
-                    <v-card-subtitle>do {{ kategorizacija.kategorija1_granica }} m<sup>3</sup> </v-card-subtitle>
+                    <v-card-subtitle>
+                        do  <span v-if="!changeActivated">{{ categories.kategorija1_granica }}</span>
+                        <v-text-field v-else v-model="categories.kategorija1_granica" class="editable-text"></v-text-field>
+                        m<sup>3</sup>
+                    </v-card-subtitle>
 
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-chip color="primary"> {{ kategorizacija.kategorija1_cijena }} KM </v-chip>
+                        <v-chip color="primary"> 
+                            <span v-if="!changeActivated">{{ categories.kategorija1_cijena }}</span>
+                            <v-text-field v-else v-model="categories.kategorija1_cijena" class="editable-text"></v-text-field>
+                            KM 
+                        </v-chip>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -40,13 +52,21 @@
                     </v-card-title>
 
                     <v-card-subtitle>
-                        od {{ kategorizacija.kategorija1_granica }} m<sup>3</sup> 
-                        do {{ kategorizacija.kategorija2_granica }} m<sup>3</sup> 
+                        od  <span v-if="!changeActivated">{{ categories.kategorija1_granica }}</span>
+                        <v-text-field v-else v-model="categories.kategorija1_granica" class="editable-text"></v-text-field>
+                        m<sup>3</sup>
+                        do  <span v-if="!changeActivated">{{ categories.kategorija2_granica }}</span>
+                        <v-text-field v-else v-model="categories.kategorija2_granica" class="editable-text"></v-text-field>
+                        m<sup>3</sup>
                     </v-card-subtitle>
 
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-chip color="primary"> {{ kategorizacija.kategorija2_cijena }} KM </v-chip>
+                        <v-chip color="primary"> 
+                            <span v-if="!changeActivated">{{ categories.kategorija2_cijena }}</span>
+                            <v-text-field v-else v-model="categories.kategorija2_cijena" class="editable-text"></v-text-field>
+                            KM 
+                        </v-chip>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -60,19 +80,28 @@
                         3. kategorija
                     </v-card-title>
 
-                    <v-card-subtitle>preko {{ kategorizacija.kategorija2_granica }} m<sup>3</sup> </v-card-subtitle>
+                    <v-card-subtitle>
+                        preko  <span v-if="!changeActivated">{{ categories.kategorija2_granica }}</span>
+                        <v-text-field v-else v-model="categories.kategorija2_granica" class="editable-text"></v-text-field>
+                        m<sup>3</sup>
+                    </v-card-subtitle>
 
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-chip color="primary"> {{ kategorizacija.kategorija3_cijena }} KM </v-chip>
+                        <v-chip color="primary"> 
+                            <span v-if="!changeActivated">{{ categories.kategorija3_cijena }}</span>
+                            <v-text-field v-else v-model="categories.kategorija3_cijena" class="editable-text"></v-text-field>
+                            KM 
+                        </v-chip>
                     </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
         
-        <v-row>
+        <v-row >
             <v-col>
-                <p>Kategorizacija aktivna od {{ kategorizacija.date_created }}</p>
+                <p v-if="!changeActivated & !isLoading">Kategorizacija aktivna od {{ categories.date_created.replaceAll("-", ".") }}.</p>
+                <p v-else class="accent--text">Unesite ograničenja i cijene za novu kategorizaciju.</p>
             </v-col>
         </v-row>
         
@@ -80,10 +109,48 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
     name: "adminConsuptionCategories",
     data: () => ({
-        kategorizacija: { id:1,	date_created:"10.2.2022", kategorija1_cijena:"0,90", kategorija2_cijena:"1,20", kategorija3_cijena:"1,60", kategorija1_granica: "15", kategorija2_granica: "25"	}
-    })
+        //categories: { id:1,	date_created:"10.2.2022", kategorija1_cijena:"0,90", kategorija2_cijena:"1,20", kategorija3_cijena:"1,60", kategorija1_granica: "15", kategorija2_granica: "25"	},
+        changeActivated: false
+    }),
+    mounted() {
+        this.fetchCategories()
+    },
+    methods: {
+        activateChange() {
+            console.log("change activated")
+            this.changeActivated = true
+        },
+        cancelChanges() {
+            this.changeActivated = false
+        },
+        fetchCategories() {
+          this.$store
+              .dispatch('adminConsuptionCategories/fetchCategories', null, {root: true})
+        },
+        changeCategories() {
+            this.$store
+                .dispatch('adminConsuptionCategories/changeCategories', this.categories, { root: true })
+                .catch( err => {
+                    console.log(err)
+                })
+                this.changeActivated = false
+        }
+        
+    },
+    computed: {
+        ...mapGetters('adminConsuptionCategories', ['isLoading', 'categories'])
+   } 
 }
 </script>
+
+<style>
+.editable-text {
+    display: inline-block !important;
+    max-width: 65px !important;
+} 
+</style>
